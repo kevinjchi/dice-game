8846c9c35dae723e681090f9f0854b0c35d9482a
kevinjc <kevinjc@uio.no>
2018-07-28 14:22:59 +0200

    change player and gamelogic updated

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -34,10 +34,25 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	diceDOM.style.display = 'block';
 	diceDOM	.src = 'dice-' + dice + '.png';
 	
-
 	//3. Update the round score IF the rolled number was NOT a 1
-	
-	
+	if (dice !== 1) {
+		// Add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		// Next player ternary operator
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+		
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+		
+		//toggle active 'on' and 'off'
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+		
+		document.querySelector('.dice').style.display = 'none';
+	}
 });
 
 
