c46482d3dcc3ddf56c3eda20c1f76e9dea0a509d
kevinjc <kevinjc@uio.no>
2018-07-28 13:59:25 +0200

    reset game logic

--- app.js
+++ app.js
@@ -7,4 +7,48 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
+Make this game into ZOMBIE DICE later
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	
+	//1. Random number
+	var dice = Math.floor(Math.random() * 6) + 1;
+	
+	//2. Display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM	.src = 'dice-' + dice + '.png';
+	
+
+	//3. Update the round score IF the rolled number was NOT a 1
+	
+	
+});
+
+
+
+//dice = Math.floor(Math.random() * 6) + 1;
+//console.log(dice);
+
+//setter
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//getter
+//var x = document.querySelector('#score-0').textContent;
+
